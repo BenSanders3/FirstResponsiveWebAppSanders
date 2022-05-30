cd290669e8b24c0300cb8aa783610f2ce65f466e
BenSanders3 <85141421+BenSanders3@users.noreply.github.com>
2022-05-29 20:03:29 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstResponsiveWebAppSanders.Models;
+
+namespace FirstResponsiveWebAppSanders.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.ATY = "";
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Index(AgeCalcModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.ATY = model.AgeThisYear();
+            }
+            else
+            {
+                ViewBag.ATY = "";
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Models/AgeCalcModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppSanders.Models
+{
+    public class AgeCalcModel
+    {
+        [Required(ErrorMessage="Please enter a name.")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Please enter a birth year.")]
+        [Range(1900, 2022, ErrorMessage = "Please enter a birth year between 1900 and 2022.")]
+        public int? BirthYear { get; set; }
+        public string AgeThisYear()
+        {
+            const int CURRENTYEAR = 2022;
+            int? EndAge = CURRENTYEAR - BirthYear;
+            return Name + " will be " + EndAge.ToString() + " years old by December 31st, 2022";
+        }
+    }
+}
